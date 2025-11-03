422a07792d402defa2574e8024a56c4ef43cc340
Andrei Ciuculescu <andrei.ciuculescu@isoftbet.com>
2025-11-04 01:11:01 +0200

    - db changes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
+if (process.env.NODE_ENV === 'production' && !process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL must be set in production to use Prisma client. Set DATABASE_URL in your Vercel environment variables.');
+}
+
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
@@ -7,4 +11,3 @@ declare global {
 
 export const prisma = global.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
-
